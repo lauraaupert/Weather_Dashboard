386ce83d8184ff5b6d0feb592363e78c7e3790a6
Laura Aupert <aupert.laura@gmail.com>
2020-12-09 15:39:13 -0500

    basic setup

---
+++ script.js
@@ -0,0 +1,15 @@
+    // This is our API key
+    var APIKey = "166a433c57516f51dfab1f7edaed8413";
+
+    // Here we are building the URL we need to query the database
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+      "q=Bujumbura,Burundi&appid=" + APIKey;
+
+      //request syntax
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+        // We store all of the retrieved data inside of an object called "response"
+        .then(function(response) {
+  
