f286472d43b31d0aff92a395a4d865e8818bbaec
Laura Aupert <aupert.laura@gmail.com>
2020-12-10 15:44:12 -0500

    with forecast request instead of weather duh

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ $(document).ready(function(){
 
         $("#search").on("click", function(event) {
             event.preventDefault();
-
             console.dir(event)
         var city = $("#city-input").val().trim();
         cities.push(city);
@@ -24,8 +23,10 @@ $(document).ready(function(){
         })
 
         function queryCity() {
+            $(".city").empty()
 
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cities[0] + "&cnt=5&appid=" + APIKey;
+           // var city = $(this).attr("data-name")
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cities[0] + "&cnt=5&appid=" + APIKey;
         $.ajax({
         url: queryURL,
         method: "GET"
@@ -33,10 +34,11 @@ $(document).ready(function(){
             console.log((response))
             //$(".display").empty()
 
-            $(".city").html("<h2>" + response.name + "</h2>");
-            $(".wind").text("Wind Speed: " + response.wind.speed);
-            $(".humidity").text("Humidity: " + response.main.humidity);
-            var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+            //WILL NEED TO REFORMAT DATE DISPLAY
+            $(".city").html("<h2>" + response.city.name + " (" + response.list[0].dt_txt + ")" + "</h2>");
+            $(".wind").text("Wind Speed: " + response.list[0].wind.speed);
+            $(".humidity").text("Humidity: " + response.list[0].main.humidity);
+            var tempF = (response.list[0].main.temp - 273.15) * 1.80 + 32;
             console.log(tempF)
             $(".temp").text("Temperature: " + tempF.toFixed(2) + "F");
     
@@ -50,15 +52,17 @@ function renderHistory() {
         
         var newCity = document.createElement("button")
         newCity.textContent = history;
-        newCity.setAttribute("data-index", i);
+        newCity.setAttribute("data-name", i);
         newCity.classList.add("list-group-item")
         newCity.classList.add("history-city")
 
         $(".history").prepend(newCity);
+        console.log($("dataset"))
     }
 
 
  }
+ 
         
  $(document).on("click", ".history-city", queryCity);
 
